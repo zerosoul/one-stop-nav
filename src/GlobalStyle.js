9e8dd57b6cbd50c10d8da2980f058017c03ee45d
Tristan <yanggc888@163.com>
2020-12-12 11:17:06 +0800

    fix: remove uesr-select

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -5,7 +5,6 @@ const GlobalStyle = createGlobalStyle`
   *{
     box-sizing:border-box;
     outline:none;
-    user-select: none;
     -webkit-text-size-adjust: none;
     -webkit-tap-highlight-color: rgba(0,0,0,0);
   }
