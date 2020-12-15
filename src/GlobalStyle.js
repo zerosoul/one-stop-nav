67c3593084c45b3a45b50d3c400cbbafd450d08f
zerosoul <yanggc888@163.com>
2020-12-15 15:23:23 +0800

    fix: appearance none

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -5,7 +5,6 @@ const GlobalStyle = createGlobalStyle`
   *{
     box-sizing:border-box;
     outline:none;
-    user-select: none;
     -webkit-text-size-adjust: none;
     -webkit-tap-highlight-color: rgba(0,0,0,0);
   }
@@ -32,6 +31,9 @@ const GlobalStyle = createGlobalStyle`
     border:none;
     background: none;
     cursor:pointer;
+  }
+  input{
+    appearance: none;
   }
   a{
     text-decoration:none;
